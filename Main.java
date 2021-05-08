b7421ec3b8d1e16ef522c5adbd5d54b641664b86
Anto <46535815+Anto098@users.noreply.github.com>
2021-05-08 10:00:14 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,20 +1,13 @@
-import TP1.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-// TP1 IFT 1025 AUTOMNE 2019
-// ANTONIN ROY 20145595
-// HUGO BAUDCHON 20071284
-public class Main {
-    public static void main(String[] args) throws IOException {
-        Engin engin = new Engin();
+import InterfaceGUI.DictionaryClasses.*;
+import InterfaceGUI.*;
 
-        while(true) {                                               // Logique du code en général, on boucle en entrant des commandes, tant que la commande n'est pas "end"
-            System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-            System.out.println("Entrez votre commande");
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            String s = br.readLine();
-            Commandes.traiterCommande(s, engin);
-        }
+public class Main {
+    public static void main(String[] args) {
+        FrameEngine frameEngine = new FrameEngine();
+
+
+
+
+
     }
 }
